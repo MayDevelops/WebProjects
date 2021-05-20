aa0c4321d1f479320c39300059dae50b6572ebca
JC May <jmay1126@AC02XW060JG5J.local>
2021-05-19 22:31:18 -0600

    Today I created the skeleton for lab 3a. The skeleton right now is just wire framing the header, buttons, and comics display section. I have begun implementing a redux store to be able to update the comic component when the buttons are pressed. I think I could of just used props, but Kumar told me to learn redux so here I am trying to learn more about redux. I think this will work well once I get the redux store opened for the other components and read some more articles on how to pass state/props between different components.

---
+++ ReactWebServer/Homepage/src/Components/ButtonSegment.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ButtonSegment = () => {
+    return (
+        <div className="buttonWrapper">
+            <div className="center">
+                <button>Previous</button>
+                <button>Next</button>
+                <button>Random</button>
+            </div>
+        </div>
+    );
+}
+
+export default ButtonSegment;
