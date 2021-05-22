c3fb9bf7854447a2004c402498c170afbf241be5
JC <jcjamesmay@gmail.com>
2021-05-21 23:54:20 -0600

    build with logo footer

---
+++ ReactWebServer/Homepage/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="github">
+            <a href="https://github.com/MayDevelops/WebProjects/tree/master/ReactWebServer/Homepage">GitHub</a>
+        </div>
+    );
+};
+
+export default Footer;
