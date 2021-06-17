1f0ad4fcf35aaf57785f7fc9bbc19fb5de1ff177
JC <jcjamesmay@gmail.com>
2021-06-17 16:20:39 -0600

    Implemented hamburger menu functionality to take the ugly forms off of the my pokemon page. Menu now implements the CRUD options for the pokedexes.

--- Pokemon/front-end/src/components/PokedexNavigator.vue
+++ Pokemon/front-end/src/components/PokedexNavigator.vue
@@ -1,15 +1,49 @@
 <template>
   <div>
-    <input v-model="message" placeholder="Pokedex Name">
-    <button type="submit" class="pure-button pure-button-primary" @click.prevent="createPokedex(message)">Submit
-    </button>
+    <Slide right>
+      <div class="sliderWrapper">
+        <div class="sliderPokedexTitle">
+          <span>Pokedexes:</span>
+        </div>
+        <div class="sliderPokedexes">
+          <b-list-group class="listGroup">
+            <b-list-group-item class="listGroupItem" button @click="selectPokedex(pokedex.name)" v-for="pokedex in allPokedexes"
+                               v-bind:key="pokedex._id">{{ pokedex.name }}
+            </b-list-group-item>
+          </b-list-group>
+        </div>
 
+        <hr>
+        <div class="sliderPokedexTitle">
+          <span>Create:</span>
+          <input v-model="createText" placeholder="Pokedex Name">
+          <button type="submit" class="createButton" @click.prevent="createPokedex(createText)">Create
+          </button>
+        </div>
 
-    <select @change="selectPokedex()" v-model="selectedPokedex">
-      <option disabled value="">Please select one</option>
-      <option v-for="pokedex in allPokedexes" v-bind:key="pokedex._id">{{ pokedex.name }}</option>
-    </select>
-    <span>Selected Pokedex: {{ currentPokedexName }}</span>
+        <hr>
+
+        <div class="sliderPokedexTitle">
+          <span>Rename:</span>
+          <input v-model="renameText" placeholder="New Pokedex Name">
+          <button type="submit" class="createButton" @click.prevent="renamePokedex(renameText)">Rename
+          </button>
+        </div>
+
+        <hr>
+
+        <div class="sliderPokedexTitle">
+          <span>Delete:</span>
+          <br>
+          <button type="submit" class="createButton" @click.prevent="deletePokedex()">Delete
+          </button>
+
+        </div>
+
+      </div>
+    </Slide>
+
+    <h1>Selected Pokedex: {{ currentPokedexName }}</h1>
 
   </div>
 
@@ -17,12 +51,18 @@
 
 <script>
 import axios from 'axios';
+import {Slide} from 'vue-burger-menu'  // import the CSS transitions you wish to use, in this case we are using `Slide`
+
 
 export default {
   name: 'PokedexNavigator',
+  components: {
+    Slide // Register your component
+  },
   data() {
     return {
-      message: '',
+      renameText: '',
+      createText: '',
       selectedPokedex: '',
     }
   },
@@ -60,17 +100,57 @@ export default {
       this.$root.$data.selector = (this.$root.$data.trainer.pokedexes.length) - 1;
       this.selectedPokedex = this.$root.$data.trainer.pokedexes[this.$root.$data.selector].name;
       this.$emit("syncDex");
-      this.message = '';
+      this.createText = '';
     },
-    async selectPokedex() {
+    async renamePokedex(message) {
+
+      this.$root.$data.trainer.pokedexes[this.$root.$data.selector].name = message;
+
+      try {
+        await axios.put('/api/trainers/pokedex', {
+          user: this.$root.$data.trainer,
+          pokedexes: this.$root.$data.trainer.pokedexes
+        });
+      } catch (error) {
+        console.log(error);
+      }
+      this.selectedPokedex = this.$root.$data.trainer.pokedexes[this.$root.$data.selector].name;
+      this.$emit("syncDex");
+      this.renameText = '';
+    },
+
+    async deletePokedex() {
+      for(let i = 0; i < this.$root.$data.trainer.pokedexes.length; i++) {
+        if(this.$root.$data.trainer.pokedexes[i].name === this.selectedPokedex) {
+          this.$root.$data.trainer.pokedexes.splice(i, 1);
+          break;
+        }
+      }
+
+      try {
+        await axios.put('/api/trainers/pokedex', {
+          user: this.$root.$data.trainer,
+          pokedexes: this.$root.$data.trainer.pokedexes
+        });
+      } catch (error) {
+        console.log(error);
+      }
+      this.$root.$data.selector = 0;
+      this.selectedPokedex = this.$root.$data.trainer.pokedexes[this.$root.$data.selector].name;
+      this.$emit("syncDex");
+
+    },
+    async selectPokedex(event) {
       //find the pokedex with the matching name as the selected pokedex
       for (let i = 0; i < this.$root.$data.trainer.pokedexes.length; i++) {
-        if (this.selectedPokedex === this.$root.$data.trainer.pokedexes[i].name) {
+        if (event === this.$root.$data.trainer.pokedexes[i].name) {
           this.$root.$data.selector = i;
           this.$emit('syncDex');
           break;
         }
       }
+      this.selectedPokedex = this.$root.$data.trainer.pokedexes[this.$root.$data.selector].name;
+
     },
   },
   async created() {
@@ -87,5 +167,102 @@ export default {
 </script>
 
 <style>
+
+.sliderWrapper {
+  width: 100%;
+}
+
+.sliderPokedexTitle {
+justify-self: left;
+  color: #F2921D;
+}
+
+.createButton {
+text-align: center;
+  width: 50%;
+  background: rgba(0,0,0,0.84);
+  color: #F2921D;
+border-radius: 10px;
+}
+
+.sliderPokedexes {
+background: #373a47;
+}
+
+.listGroup {
+}
+
+.listGroupItem {
+
+}
+
+.listGroupItem:hover {
+}
+
+.bm-burger-button {
+  position: fixed;
+  width: 36px;
+  height: 30px;
+  left: 36px;
+  top: 36px;
+  cursor: pointer;
+}
+.bm-burger-bars {
+  background-color: #373a47;
+}
+.line-style {
+  position: absolute;
+  height: 20%;
+  left: 0;
+  right: 0;
+}
+.cross-style {
+  position: absolute;
+  top: 12px;
+  right: 2px;
+  cursor: pointer;
+}
+.bm-cross {
+  background: #F2921D;
+
+
+
+}
+.bm-cross-button {
+  height: 24px;
+  width: 24px;
+}
+.bm-menu {
+  height: 100%; /* 100% Full-height */
+  width: 0; /* 0 width - change this with JavaScript */
+  position: fixed; /* Stay in place */
+  z-index: 1000; /* Stay on top */
+  top: 0;
+  left: 0;
+  background-color: rgb(63, 63, 65); /* Black*/
+  overflow-x: hidden; /* Disable horizontal scroll */
+  padding-top: 60px; /* Place content 60px from the top */
+  transition: 0.5s; /*0.5 second transition effect to slide in the sidenav*/
+}
+
+.bm-overlay {
+  background: rgba(0, 0, 0, 0.3);
+}
+.bm-item-list {
+  color: #b8b7ad;
+  font-size: 20px;
+  margin:0;
+
+}
+.bm-item-list > * {
+  display: inline-block;
+  text-decoration: none;
+  margin:0;
+align-content: flex-start;
+}
+.bm-item-list > * > span {
+  font-weight: 700;
+  color: white;
+}
 
 </style>
